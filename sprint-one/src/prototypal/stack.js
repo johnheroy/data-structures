5eefdfcc2f1a11997e6ad60131241c94af324197
johnheroy <johnheroy@gmail.com>
2014-09-11 11:25:29 -0700

    Implement prototypal makeStack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,28 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(stackMethods);
+  obj.storage = {};
+  obj.length = 0;
+  return obj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(item){
+    this.storage[this.length] = item;
+    this.length++;
+  },
+  pop: function(){
+    if (this.length > 0){
+      var temp = this.storage[this.length-1];
+      delete this.storage[this.length-1];
+      this.length--;
+      return temp;
+    }
+  },
+  size: function(){
+    return this.length;
+  }
+};
 
 
