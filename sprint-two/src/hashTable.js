22f843acc762d90d3700c197a22f045f0e277af9
johnheroy <johnheroy@gmail.com>
2014-09-11 16:40:25 -0700

    Implement hash table sans working remove

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,33 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var temp = {};
+  temp[k] = v;
+  if (!this._storage[i]){
+    this._storage[i] = [temp];
+  } else {
+    this._storage[i].push(temp);
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var bucket = this._storage[i];
+  if (bucket){
+    for (var j = 0; j < bucket.length; j++){
+      var currentItem = bucket[j];
+      if (currentItem[k]){
+        return currentItem[k];
+      }
+    }
+  }
+  return null;
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  // problematic
+  delete this._storage[i];
 };
 
 
