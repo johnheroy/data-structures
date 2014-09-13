c081b8f84a66316a3a539cdb54532ff35e777ce9
johnheroy <johnheroy@gmail.com>
2014-09-12 21:51:05 -0700

    Implement tree.flatten

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,25 +1,36 @@
-var makeBinarySearchTree = function(value){
+var makeBinarySearchTree = function(value, parent){
   var tree = {};
 
   tree.value = value;
+  tree.parent = parent;
   tree.left = null;
   tree.right = null;
+  tree.height = 1;
 
   tree.insert = function(item){
     if (item >= tree.value) {
-      if (!tree.right) {
-        tree.right = makeBinarySearchTree(item);
+      if (!tree.right){
+        tree.right = makeBinarySearchTree(item, tree).bubble();
       } else {
         tree.right.insert(item);
       }
     } else {
-      if (!tree.left) {
-        tree.left = makeBinarySearchTree(item);
+      if (!tree.left ){
+        tree.left  = makeBinarySearchTree(item, tree).bubble();
       } else {
         tree.left.insert(item);
       }
     }
   };
+
+  tree.bubble = function(){
+    if (tree.parent){
+      tree.parent.height = tree.height + 1;
+      tree.parent.bubble();
+    }
+    return tree;
+  };
+
   tree.contains = function(item){
     if (item === tree.value) {
       return true;
@@ -27,9 +38,9 @@ var makeBinarySearchTree = function(value){
       return (tree.right) ? tree.right.contains(item) : false;
     } else {
       return (tree.left) ? tree.left.contains(item) : false;
-
     }
   };
+
   tree.depthFirstLog = function(callback){
     callback(tree.value);
     if (tree.right) {
@@ -38,6 +49,28 @@ var makeBinarySearchTree = function(value){
     if (tree.left){
       tree.left.depthFirstLog(callback);
     }
+  };
+
+  tree.flatten = function(){
+    var results = [];
+
+    var left = (tree.left) ? tree.left.flatten() : null;
+    if (left){
+      for (var i = 0; i < left.length; i++){
+        results.push(left[i]);
+      }
+    }
+
+    results.push(tree.value);
+
+    var right = (tree.right) ? tree.right.flatten() : null;
+    if (right){
+      for (var i = 0; i < right.length; i++){
+        results.push(right[i]);
+      }
+    }
+
+    return results;
   };
 
   return tree;
