b5de4e923453323ab4af33f66fbdf2471b1a0650
johnheroy <johnheroy@gmail.com>
2014-09-12 21:53:11 -0700

    Add additional tree specs

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -1,8 +1,21 @@
 describe('binarySearchTree', function() {
   var binarySearchTree;
+  var bigTree;
+  var smallTree;
 
   beforeEach(function() {
     binarySearchTree = makeBinarySearchTree(5);
+
+    bigTree = makeBinarySearchTree(10);
+    bigTree.insert(20);
+    bigTree.insert(2);
+    bigTree.insert(3);
+    bigTree.insert(4);
+    bigTree.insert(5);
+
+    smallTree = makeBinarySearchTree(2);
+    smallTree.insert(1);
+    smallTree.insert(3);
   });
 
   it('should have methods named "insert", "contains", and "depthFirstLog', function() {
@@ -38,39 +51,37 @@ describe('binarySearchTree', function() {
     expect(array).to.eql([5,2,3]);
   });
 
-  it('should contain "maxDepth" and "minDepth" properties', function(){
-    expect(binarySearchTree.hasOwnProperty("maxDepth").to.equal(true));
-    expect(binarySearchTree.hasOwnProperty("minDepth").to.equal(true));
+  it('should contain "parent" property', function(){
+    expect(binarySearchTree.hasOwnProperty('parent')).to.equal(true);
+  });
+
+  it('should reference parent if it exists', function(){
+    expect(smallTree.right.parent).to.equal(smallTree);
+  });
+
+  it('should contain "height" property', function(){
+    expect(binarySearchTree.hasOwnProperty("height")).to.equal(true);
+  });
+
+  it('should change height values after adding a new node', function(){
+    expect(smallTree.height).to.equal(2);
+  });
+
+  it('should have a "flatten" method which returns a sorted array of elements', function(){
+    expect(smallTree.flatten()).to.eql([1, 2, 3]);
   });
 
   it('should rebalance the tree when the max depth ' +
-     'is greater than twice the min depth', function(){
-    binarySearchTree.insert(10);
-    binarySearchTree.insert(20);
-    binarySearchTree.insert(2);
-    binarySearchTree.insert(3);
-    binarySearchTree.insert(4);
-    binarySearchTree.insert(5);
-    expect(binarySearchTree.value).to.equal(5);
-    expect(binarySearchTree.left.value).to.equal(3);
-    expect(binarySearchTree.left.left.value).to.equal(2);
-    expect(binarySearchTree.left.right.value).to.equal(4);
-    expect(binarySearchTree.right.value).to.equal(20);
-    expect(binarySearchTree.right.left.value).to.equal(10);
+     'is greater than or equal to twice the min depth', function(){
+    expect(bigTree.value).to.equal(5);
+    expect(bigTree.left.value).to.equal(3);
+    expect(bigTree.left.left.value).to.equal(2);
+    expect(bigTree.left.right.value).to.equal(4);
+    expect(bigTree.right.value).to.equal(20);
+    expect(bigTree.right.left.value).to.equal(10);
   });
 
 });
-
-
-
-
-// Track max/ min depth
-// fire rebuild function when max changes
-//
-
-
-
-
 
 
 
